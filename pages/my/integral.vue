9e67eba6669756cf66ca0ad7973835015118df4b
wu0211 <wbs_211@163.com>
2020-11-25 09:08:54 +0800

    我的积分

---
+++ pages/my/integral.vue
@@ -0,0 +1,21 @@
+
+<!-- 我的积分 -->
+<template>
+	<view>
+		
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
