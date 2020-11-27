513fbc895b2cfa20662816340e59017db4c0a5f6
wu0211 <wbs_211@163.com>
2020-11-27 16:18:37 +0800

    积分订单

--- pages/my/integralOrder.vue
+++ pages/my/integralOrder.vue
@@ -33,43 +33,47 @@
 				</scroll-view>
 			</swiper-item>
 			<swiper-item>
-				<view class="list">
-					<view v-for="(item,index) in [1,2,3]" :key="index" class="item">
-						<view class="order_info flexbetween">
-							<view class="code">
-								订单编号：202005081325283003
+				<scroll-view scroll-y="true" style="height:100%">
+					<view class="list">
+						<view v-for="(item,index) in [1,2,3]" :key="index" class="item">
+							<view class="order_info flexbetween">
+								<view class="code">
+									订单编号：202005081325283003
+								</view>
+								<view class="type">
+									已发货
+								</view>
 							</view>
-							<view class="type">
-								已发货
+							<GoodsShow navigateUrl=""></GoodsShow>
+							<view class="yfh_button flexend">
+								<button type="default">查看物流</button>
+								<button type="default">确认收货</button>
 							</view>
-						</view>
-						<GoodsShow navigateUrl=""></GoodsShow>
-						<view class="yfh_button flexend">
-							<button type="default">查看物流</button>
-							<button type="default">确认收货</button>
 						</view>
 					</view>
-				</view>
+				</scroll-view>
 			</swiper-item>
 			<swiper-item>
-				<view class="list">
-					<view v-for="(item,index) in [1,2,3]" :key="index" class="item">
-						<view class="order_info flexbetween">
-							<view class="code">
-								订单编号：202005081325283003
+				<scroll-view scroll-y="true" style="height:100%">
+					<view class="list">
+						<view v-for="(item,index) in [1,2,3]" :key="index" class="item">
+							<view class="order_info flexbetween">
+								<view class="code">
+									订单编号：202005081325283003
+								</view>
+								<view class="type">
+									已签收
+								</view>
 							</view>
-							<view class="type">
-								已签收
-							</view>
-						</view>
-						<GoodsShow navigateUrl=""></GoodsShow>
+							<GoodsShow navigateUrl=""></GoodsShow>
 
-						<view class="yfh_button flexend">
-							<button type="default">删除订单</button>
-							<button type="default">评价</button>
+							<view class="yfh_button flexend">
+								<button type="default">删除订单</button>
+								<button type="default">评价</button>
+							</view>
 						</view>
 					</view>
-				</view>
+				</scroll-view>
 			</swiper-item>
 		</swiper>
 	</view>
@@ -96,20 +100,19 @@
 </script>
 
 <style scoped lang="scss">
-	
-	
 	// .inegralOrder {
 	// 	position: relative;
 	// }
 
 	.nav {
-		// position: fixed;
-		// top: 0;
-		// left: 0;
-		// right: 0;
+		position: fixed;
+		top: var(--window-top);
+		left: 0;
+		right: 0;
 		display: flex;
 		border-bottom: 2rpx solid #F5F5F5;
-		background-color: white;
+		background: white;
+		z-index: 1;
 
 	}
 
@@ -121,7 +124,9 @@
 		font-weight: 500;
 		color: #000000;
 		cursor: pointer;
+		background-color: #FFFFFF;
 		position: relative;
+
 	}
 
 	.select:after {
@@ -137,6 +142,7 @@
 
 	swiper {
 		padding-top: 66rpx;
+		box-sizing: border-box;
 	}
 
 	.item {
@@ -186,8 +192,7 @@
 			padding: 16rpx 32rpx;
 			margin: 0;
 			font-size: 28rpx;
-			width: 176rpx;
-			height: 68rpx;
+
 			line-height: 32rpx;
 			border-radius: 0 !important;
 		}
@@ -197,8 +202,5 @@
 			background-color: #2B2C3E !important;
 			margin-left: 48rpx;
 		}
-
-
-
 	}
 </style>
