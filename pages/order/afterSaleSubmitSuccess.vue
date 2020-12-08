b4120c9617fad412eedd013583bdf468449078c1
wu0211 <wbs_211@163.com>
2020-12-08 15:59:04 +0800

    售后提交成功

---
+++ pages/order/afterSaleSubmitSuccess.vue
@@ -0,0 +1,81 @@
+<template>
+	<view class="submit_wrap">
+		<view class="flex-column-center">
+			<image src="" mode="aspectFill"></image>
+			<text class="status_text">提交成功</text>
+		</view>
+
+		<view class="btn">
+			<view class="">
+				返回首页
+			</view>
+			<view class="">
+				查看状态
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.submit_wrap {
+		height: 100%;
+		width: 100%;
+		padding: 32rpx;
+		box-sizing: border-box;
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+			
+			image{
+				margin-top: 188rpx;
+				width: 210rpx;
+				height: 210rpx;
+			}
+		.status_text {
+			text-align: center;
+			font-size: 40rpx;
+			font-family: PingFangSC-Regular, PingFang SC;
+			font-weight: 400;
+			color: #2B2C3E;
+			line-height: 56rpx;
+
+		}
+
+		.btn {
+			margin-top: 242rpx;
+			display: flex;
+
+			view {
+				width: 326rpx;
+				height: 104rpx;
+				font-size: 40rpx;
+				font-weight: 400;
+				line-height: 104rpx;
+				border: 2rpx solid #2B2C3E;
+				text-align: center;
+			}
+
+			view:nth-child(1) {
+				color: #2B2C3E;
+				margin-right: 17rpx;
+			}
+
+			view:nth-child(2) {
+				margin-left: 17rpx;
+				background-color: #2B2C3E;
+				color: #FFFFFF;
+			}
+		}
+	}
+</style>
